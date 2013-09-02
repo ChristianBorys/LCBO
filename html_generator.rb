9642f983c61e6b41420019f50192e20cbbff974c
inframatic <isteffy@hotmail.com>
2013-09-02 17:25:16 -0400

    all

--- html_generator.rb
+++ html_generator.rb
@@ -1,73 +1,73 @@
-require "json"
-require "open-uri"
+require 'json'
+require 'open-uri'
 
 class HtmlGenerator
-		attr_accessor :products, :html_start, :html_end	
-	
-		def initialize 
-				raw_response = open("http://lcboapi.com/products.read") 
-				parsed_response = JSON.parse(raw_response)
+	attr_accessor :products, :html_start, :html_end
 
-				@products = parsed_response["result"]
-				
-			
-			@html_start = [
-					"<!DOCTYPE HTML>\n",
-					"<html lang='en'>\n"
-					"<head>\n", "<link rel='stylesheet' type=text/css' href='style.css'>\n",
-					"<link rel='stylesheet' type='text/css' href='reset.css'>\n",
-					"<title>Alcool</title>\n",
-					"</head>\n",
-					"<body>\n"
-			]
-		
-			@html_end = [
-					"</div> <! -- close product div -->\n",
-					"</div> <!-- close container div -->\n",
-					"<div id = 'footer'> Bringing You Happiness Since 1927</div> <!-- close footer div -->\n",
-					"</body>\n"
-					"</html>\n"
-			]
+	def initialize
+		raw_response = open("http://lcboapi.com/products").read
 
-		end
+		# Parse JSON-formatted text into a Ruby Hash
+		parsed_response = JSON.parse(raw_response)
+
+		# Return the actual result data from the response, 
+		# ignoring metadata
+		#@products = parsed_response["result"].first["id"]
+		@products   = parsed_response["result"]
+
+		@html_start = [ 
+			"<!DOCTYPE HTML>\n", 
+			"<html lang='en'>\n", 
+			"<head>\n", "<link rel='stylesheet' type='text/css' href='style.css'>\n",
+			"<link rel='stylesheet' type='text/css' href='reset.css'>\n",
+			"<title>LCBO</title>\n",
+			"</head>\n",
+			"<body>\n"
+		]
+
+		@html_end = [
+		"</div> <!-- close product div -->\n",
+		"</div>  <!-- close container div -->\n",
+		"<div id='footer'>Good Times with Better Friends</div> <!-- close footer div -->\n",
+		"</body>\n",
+		"</html>\n"
+		]
+
+	end
 
 
-		def index
-			@html_start.each { |tag| puts tag}
-			puts "<div id = 'container'>"
-			puts 	"<div id = 'header'>Liquor Control Board of Assholes</div><!-- close header div -->"
-			puts 	"<div class= 'products'>"
+	def index
+		@html_start.each { |tag| puts tag}
+		puts "<div id='container'>"
+		puts	"<div id='header'>Patio Season</div><!-- close header div -->" 
+		puts	"<div class='products'>"
 
-			puts "<div class='container2'>"
-
-			@products.each do |product| 
-					if product.has_key?("image_thumb_url") && product.has_key?("id")
-							puts "<div class='product_img'>"
-									puts "<div class ='id'>"
-									puts "#{product['id']}"
-									puts "</div>"
-							puts "<img src='#{product["image_thumg_url"]}'>"
-									puts "<div class='product_name'>"
-									puts "#{product['name']}"
-									puts "</div>"
-							puts "</div>"
-					end
+		puts "<div class='container2'>"
+		@products.each do |product| 
+			if product.has_key?("image_thumb_url") && product.has_key?("id")
+				# puts "<div class='id'>#{product['id']}</div>"
+				puts "<div class='product_img'> "
+					puts " <div class='id'> "
+					puts " #{product['id']} "
+					puts "</div>"
+				puts " <img src='#{product["image_thumb_url"]}'> "
+					puts "<div class='product_name'>"
+					puts "#{product['name']}"
+					puts "</div>"
+				puts "</div>"
 			end
-			puts "</div>"
-			@html_end.each {|tag| puts tag}
 		end
+		puts "</div>"
+		@html_end.each { |tag| puts tag }
+	end
 
-
-
-
-		def show(product_id)
-				@products.each do |product|
-						if product.fetch("id") == product_id.the_product_id	
-								product.each do |key, value|
-										puts "#{key} : #{value}"
-								end
-						end
+	def show(product_id)
+		@products.each do |product|
+			if product.fetch("id") == product_id.to_i
+				product.each do |key, value|
+					puts "#{key} : #{value}"
 				end
+			end
 		end
+	end
 end
-	
